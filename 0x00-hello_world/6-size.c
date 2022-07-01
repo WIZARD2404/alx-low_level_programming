dc5b634fca87357baad8808e5e412dc6536e4022
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-01 17:25:35 +0100

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print ("Size of a char: %d byte\(s\)", sizeof(char));
+print ("Size of an int: %d byte\(s\)", sizeof(int));
+print ("Size of a long int: %d byte\(s\)", sizeof(long int));
+print ("Size of a long long int: %d byte\(s\)", sizeof(long long int));
+print ("Size of an int: %d byte\(s\)", sizeof(float));
+return 0;
+}
