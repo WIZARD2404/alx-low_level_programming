0a25961cc093a64ac84d0986d5840f76d085f431
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-04 22:55:56 +0100

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+		{
+		if (c == q || c == e)
+			break;
+
+		putchar(c);
+
+		}
+
+	putchar('\n');
+
+	return (0);
+}
