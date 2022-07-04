5276d0b0f87fd637588dea4b1f5e102a3b4b0334
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-04 23:01:58 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - 
+ * main - Printes alphabets except 'q' and 'e'
  * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-
-		{
+	for (c = 'a'; c <= 'z'; c++){
 		if (c != 'q' && c != 'e')
 			putchar(c);
-
 		}
-
 	putchar('\n');
-
 	return (0);
 }
