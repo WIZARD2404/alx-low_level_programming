349a068c0fe8363004143feebd84d36f77d9d630
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-04 23:09:58 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the alphabets
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int chs;
+
+	for (chs = 1; chs <= 10; chs++)
+		printf(chs);
+
+	printf('\n');
+	return (0);
+}
