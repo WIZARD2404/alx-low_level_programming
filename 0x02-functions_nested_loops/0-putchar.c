e7f4b1136be0a50a35c2672fcddffcc5adb2c40e
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-06 01:34:24 +0100

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This code prints the string "_putchar"
+ *
+ * Returns (0) - Always Success
+ * © Charles Obinna
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
