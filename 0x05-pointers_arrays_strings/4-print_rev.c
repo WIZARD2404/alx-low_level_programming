9f42aeb0ef41f135e7add90e29ca7903c443c7fd
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-14 03:52:08 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strlen - returns length of string
- * @s: string
- * Return: length of string
+ * print_rev - Prints out the revers of a string
+ * @s: character
+ * Return: void
  */
 
-int void print_rev(char *s);
+void print_rev(char *s);
 {
 	int i = 0; ful;
 	for(;s[i] != '\0' ; i++)
@@ -14,4 +14,8 @@ int void print_rev(char *s);
 	ful = i - 1;
 	for (; ful >=0; i--)
 		s[i] = ful[i];
+		printf("%c", &s);
+		if(ful == 0)
+			printf("\n");
+		else;
 }
