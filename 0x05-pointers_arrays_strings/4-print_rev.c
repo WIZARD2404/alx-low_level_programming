f327f71403615e551b04a3b5bd193a6f284ebad7
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-14 03:47:26 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int void print_rev(char *s);
+{
+	int i = 0; ful;
+	for(;s[i] != '\0' ; i++)
+	;
+	ful = i - 1;
+	for (; ful >=0; i--)
+		s[i] = ful[i];
+}
