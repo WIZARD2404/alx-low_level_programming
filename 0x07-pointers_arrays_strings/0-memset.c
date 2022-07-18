98096a238ce4942fc2ba7a0fb6c37610d41166fd
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-19 00:46:23 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * _memset - ironnou
- * @s: pointer to char params
- * @b: data to change
- * @n: index
+ * _memset - function fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * @s: pointer
+ * @b: Const
+ * @n: counter
  * Return: *s
  */
 char *_memset(char *s, char b, unsigned int n)
