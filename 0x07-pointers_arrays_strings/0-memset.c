61e506efa5be67ef5c612c678971e1908b62c50c
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-19 00:43:16 +0100

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - ironnou
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+        int i = 0;
+        while(i < n)
+        {
+                *(s + i) = b;
+                i++;
+        }
+        return (s);
+}
