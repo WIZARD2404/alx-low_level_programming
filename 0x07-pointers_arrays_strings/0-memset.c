1bf84a5fb22bfd96bc9bcfa25ddfed80e057e14e
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-19 01:45:48 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i = 0;
-        while(i < n)
-        {
-                *(s + i) = b;
-                i++;
-        }
-        return (s);
+	unsigned int i = 0;
+
+	while(i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
 }
