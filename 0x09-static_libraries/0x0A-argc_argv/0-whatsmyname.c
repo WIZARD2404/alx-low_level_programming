bca3e07ea4c808bb973cfbd13c3dfb8abe37be18
Charles Obinna <65284020+WIZARD2404@users.noreply.github.com>
2022-07-23 03:55:10 +0100

    Create 0-whatsmyname.c

---
+++ 0x09-static_libraries/0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * @argc: A integer containing the total number of command and its flags
+ *
+ * @argv: An array containing the number of arguments, name of file and a NULL value
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s", *(argv[0]));
+	return (0);
+}
